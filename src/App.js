3bf69ea2f8a2483eb02b6880a13aa5c95e0c5456
benedyczak <107350740+benedyczak@users.noreply.github.com>
2023-01-26 15:37:08 +0100

    React work

--- src/App.js
+++ src/App.js
@@ -1,23 +1,26 @@
-import logo from './logo.svg';
+import React, { createContext, useState } from "react";
 import './App.css';
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
+        <img src="https://www.referenseo.com/wp-content/uploads/2019/03/image-attractive.jpg" className="App-logo" alt="logo" />
+        <p> Léonard Bénédyczak</p>
         <p>
           Edit <code>src/App.js</code> and save to reload.
         </p>
         <a
           className="App-link"
-          href="https://reactjs.org"
+          href="https://www.google.com/"
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React
+          Lien vers Google
         </a>
+        <button>Cliquez</button>
       </header>
+
     </div>
   );
 }
